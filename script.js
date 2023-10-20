bc109313f16020cda2590de236c894d7f38b0f25
Vaskya Nabila <83712719+pillnb@users.noreply.github.com>
2023-10-20 12:09:16 +0700

    upppdate

---
+++ script.js
@@ -0,0 +1,11 @@
+    let nameInput = doccument.getElementById("name");
+    let emailInput = doccument.getElementById("email");
+    let passwordInput = doccument.getElementById("password");
+    let nrpInput = doccument.getElementById("nrp");
+    let dateInput = doccument.getElementById("date");
+ 
+    const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+function validation() {
+
+}
