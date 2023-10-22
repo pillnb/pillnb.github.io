6b043b7aa652b7a780796082b60e55a9d19edd9c
Vaskya Nabila <83712719+pillnb@users.noreply.github.com>
2023-10-22 13:46:35 +0700

    last update

--- script.js
+++ script.js
@@ -1,11 +1,46 @@
-    let nameInput = doccument.getElementById("name");
-    let emailInput = doccument.getElementById("email");
-    let passwordInput = doccument.getElementById("password");
-    let nrpInput = doccument.getElementById("nrp");
-    let dateInput = doccument.getElementById("date");
- 
-    const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
-function validation() {
+const form = document.getElementById('form');
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const dateInput = document.getElementById('date');
+const nrpInput = document.getElementById('nrp');
 
-}
+form.addEventListener('submit', function (e) {
+    let isValid = true;
+
+    // Validate Full Name
+    if (nameInput.value.trim() === '') {
+        alert('Full Name is required');
+        isValid = false;
+    }
+
+    // Validate Email Address
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(emailInput.value)) {
+        alert('Invalid Email Address');
+        isValid = false;
+    }
+
+    // Validate Password
+    if (passwordInput.value.length < 8) {
+        alert('Password must be at least 8 characters');
+        isValid = false;
+    }
+
+    // Validate Birth Date (You can add your own validation logic)
+    if (dateInput.value.trim() === '') {
+        alert('Birth Date is required');
+        isValid = false;
+    }
+
+    // Validate Referral Code
+    if (nrpInput.value.trim() === '' || isNaN(nrpInput.value) || nrpInput.value.length !== 10) {
+        alert('Invalid Referral Code. It should be a 10-digit number');
+        isValid = false;
+    }
+
+    if (!isValid) {
+        e.preventDefault(); // Prevent form submission
+    }
+});
